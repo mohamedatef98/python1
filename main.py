1fe080eec76a5a7e266a9d90149f2f8f845efebe
mohamedatef98 <31374701+mohamedatef98@users.noreply.github.com>
2017-09-22 21:53:15 +0200

    stop at any field update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 persons=[]
+fields = ['name', 'email','mobile','university','major']
 id = 1
 while 1:
     person={
@@ -10,20 +11,39 @@ while 1:
             'major':''
             }
     person['name']=input('Enter The name')
-    if person['name'].upper()[0]=="S" and person['name'].upper()[len(person['name'])-1] =="P":
+    if person['name'].upper()=="STOP":
         break
     person['email'] = input('Enter The email')
+    if person['email'].upper() == "STOP":
+        person['email'] = ''
+        persons.append(person)
+        print("New Person added with name : "+person['name']+" and an email : "+person['email']+" and a mobile : "+person['mobile']+" at university : "+person       ['university']+" at the major : "+person['major'])
+        break
     person['mobile'] = input('Enter The mobile')
+    if person['mobile'].upper() == "STOP":
+        person['mobile'] = ''
+        persons.append(person)
+        print("New Person added with name : "+person['name']+" and an email : "+person['email']+" and a mobile : "+person['mobile']+" at university : "+person       ['university']+" at the major : "+person['major'])
+        break
     person['university'] = input('Enter The university')
+    if person['university'].upper() == "STOP":
+        person['university'] = ''
+        persons.append(person)
+        print("New Person added with name : "+person['name']+" and an email : "+person['email']+" and a mobile : "+person['mobile']+" at university : "+person       ['university']+" at the major : "+person['major'])
+        break
     person['major'] = input('Enter The major')
+    if person['major'].upper() == "STOP":
+        person['major'] = ''
+        persons.append(person)
+        print("New Person added with name : "+person['name']+" and an email : "+person['email']+" and a mobile : "+person['mobile']+" at university : "+person       ['university']+" at the major : "+person['major'])
+        break
     persons.append(person)
     print("New Person added with name : "+person['name']+" and an email : "+person['email']+" and a mobile : "+person['mobile']+" at university : "+person['university']+" at the major : "+person['major'])
 
 outputFile = open('names.csv', 'w')
 with outputFile:
-    writer = csv.DictWriter(outputFile, fieldnames=['name', 'email','mobile','university','major'])
+    writer = csv.DictWriter(outputFile, fieldnames=fields)
     writer.writeheader()
     for person in persons:
         writer.writerow(person)
-
 
